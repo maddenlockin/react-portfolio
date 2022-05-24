02466f5bab597cd7a32fe9c182e00f7e82538856
maddenlockin <maddenlockin@gmail.com>
2022-05-24 14:38:17 -0700

    put anchors into Header component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import Experience from './Experience';
+import Projects from './Projects';
+import TechStack from './TechStack';
+
 
 export default function Header() {
   return (
-    <div></div>
+    <>
+      <a className="navbar" href={Projects}>
+        Projects
+      </a>
+      <a className="navbar" href={Experience}>
+        Experience
+      </a>
+      <a className="navbar" href={TechStack}>
+        TechStack
+      </a>
+    </>
   );
 }
