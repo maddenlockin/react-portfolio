502b27024b92cdb9200a10fa3c928f76b08c989f
maddenlockin <maddenlockin@gmail.com>
2022-05-24 14:35:41 -0700

    initalized components projects, experience, techStack, header

---
+++ src/Components/Experience.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Experience() {
+  return (
+    <>
+      <h3>Experience</h3>
+      <div className="job-container">
+        <h5 className="employer">Alchemy Code Lab</h5>
+        <p className="position">Software Engineering Teaching Assistant</p>
+        <p className="job-description">
+          Aided in instructing students during remote classes in full-stack
+          Javascript. Performed daily code reviews and debugging. Provided
+          coaching and guidance through documentation for new frameworks and
+          libraries. Co-led the student onboarding week wherein students learned
+          how to use dev tools Slack, Zoom, Github and VSCode. Performed live
+          coding demos, walking through basic website creation with HTML, CSS,
+          and JavaScript
+        </p>
+      </div>
+    </>
+  );
+}
---
+++ src/Components/Projects.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Projects() {
+  return (
+    <>
+      <h3>Projects</h3>
+    </>
+  );
+}
